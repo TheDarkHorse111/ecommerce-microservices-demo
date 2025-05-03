72b086a007ee62d30e95e1449901cccb4a0641f2
Suliman Abu Zaid <suliman.abuzaid.business@gmail.com>
2025-05-03 22:23:34 +0300

    add db schema for PurchaseOrder

--- order/src/main/resources/schema.sql
+++ order/src/main/resources/schema.sql
@@ -3,7 +3,6 @@ CREATE TABLE IF NOT EXISTS purchase_order
     id         bigserial primary key,
     cart_id    bigint        not null,
     total      numeric(5, 2) NOT NULL,
-    payed_at   TIMESTAMP     NOT NULL,
     created_at TIMESTAMP     NOT NULL,
     updated_at TIMESTAMP,
     status     varchar(255)  NOT NULL
